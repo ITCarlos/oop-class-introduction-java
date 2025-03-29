f19ffe0635695bf62a692098760f3488c560f8b1
unknown <carlosafdev@gmail.com>
2025-03-28 21:31:23 -0300

    Class Triangle implemented

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,40 +1,43 @@
 package application;
+
 import java.util.Locale;
 import java.util.Scanner;
+
+import entities.Triangle;
 
 public class Main {
 
 	public static void main(String[] args) {
-		
+
 		Locale.setDefault(Locale.US);
 		Scanner input = new Scanner(System.in);
-		double xA, xB, xC, yA, yB, yC;
-		
+
+		Triangle x, y;
+		x = new Triangle();
+		y = new Triangle();
+
 		System.out.println("Enter the measures of triangle X: ");
-		xA = input.nextDouble();
-		xB = input.nextDouble();
-		xC = input.nextDouble();
+		x.a = input.nextDouble();
+		x.b = input.nextDouble();
+		x.c = input.nextDouble();
 		System.out.println("Enter the measures of triangle Y: ");
-		yA = input.nextDouble();
-		yB = input.nextDouble();
-		yC = input.nextDouble();
-		
-		double p = (xA + xB + xC)/2;
-		double areaX = Math.sqrt(p*(p-xA)*(p-xB)*(p-xC));
-				
-		p = (yA + yB + yC)/2;
-		double areaY = Math.sqrt(p*(p-yA)*(p-yB)*(p-yC));
-		
+		y.a = input.nextDouble();
+		y.b = input.nextDouble();
+		y.c = input.nextDouble();
+
+		double areaX = x.area();
+		double areaY = y.area();
+
 		System.out.printf("Triangle X area: %.4f%n", areaX);
 		System.out.printf("Triangle Y area: %.4f%n", areaY);
-		
+
 		if (areaX > areaY) {
 			System.out.println("Large area: X");
 		}
-		else { 
+		else {
 			System.out.println("Large are: Y");
 		}
-		
+
 		input.close();
 
 	}
